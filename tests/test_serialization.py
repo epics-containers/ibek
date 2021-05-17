79525107eb8015d0886540780f58b85be48027ab
Giles Knap <giles.knap@diamond.ac.uk>
2021-05-17 13:08:09 +0100

    tidy pmac.ibek.yaml and make test_serialization work

--- tests/test_serialization.py
+++ tests/test_serialization.py
@@ -1,29 +1,107 @@
 from pathlib import Path
 
+from apischema.utils import Undefined
 from ruamel.yaml import YAML
 
-from ibek.support import Database, Entity, ObjectArg, StrArg, Support
+from ibek.support import Database, Entity, IntArg, ObjectArg, StrArg, Support
 
 SUPPORT = Support(
     module="pmac",
     entities=(
         Entity(
-            name="PMAC",
-            args=(StrArg(name="PORT", description="Asyn port name", is_id=True),),
-            script="PMACPortConfigure({{ PORT }}, 100)​",
+            name="Geobrick",
+            args=(
+                StrArg(
+                    name="name",
+                    description="Name to use for the geobrick's asyn port",
+                    type="str",
+                    default=Undefined,
+                    is_id=True,
+                ),
+                ObjectArg(
+                    name="port",
+                    description="Asyn port name for pmacAsynIPPort to connect to",
+                    type="pmac.pmacAsynIPPort",
+                    default=Undefined,
+                ),
+                StrArg(
+                    name="P",
+                    description="PV Prefix for all pmac db templates",
+                    type="str",
+                    default=Undefined,
+                    is_id=False,
+                ),
+                IntArg(
+                    name="idlePoll",
+                    description="Idle Poll Period in ms",
+                    type="int",
+                    default=Undefined,
+                ),
+                IntArg(
+                    name="movingPoll",
+                    description="Moving Poll Period in ms",
+                    type="int",
+                    default=Undefined,
+                ),
+            ),
+            databases=(
+                Database(
+                    file="pmacController.template",
+                    include_args=(),
+                    define_args="PMAC={{ P }}",
+                ),
+                Database(
+                    file="pmacStatus.template",
+                    include_args=(),
+                    define_args="PMAC={{ P }}",
+                ),
+            ),
+            script=(
+                "pmacCreateController({{name}}, {{port.port}}, 0, 8, {{movingPoll}}, {{idlePoll}})",
+                "pmacCreateAxes({{name}}, 8)",
+            ),
+        ),
+        Entity(
+            name="pmacAsynIPPort",
+            args=(
+                StrArg(
+                    name="port",
+                    description="Asyn port name",
+                    type="str",
+                    default=Undefined,
+                    is_id=True,
+                ),
+                StrArg(
+                    name="IP",
+                    description="IP address of pmac",
+                    type="str",
+                    default=Undefined,
+                    is_id=False,
+                ),
+            ),
+            databases=(),
+            script=(
+                'PMACAsynIPPort({{port}}, {{IP + "" if ":" in IP else IP + ":1025"}})',
+            ),
         ),
         Entity(
             name="motor",
             args=(
                 ObjectArg(
-                    name="PMAC", type="pmac.PMAC", description="PMAC to attach to"
+                    name="pmac",
+                    description="PMAC to attach to",
+                    type="pmac.name",
+                    default=Undefined,
                 ),
             ),
             databases=(
                 Database(
-                    file="pmac_asyn_motor.template", define_args="PORT={{ PMAC.PORT }}"
+                    file="pmac_asyn_motor.template",
+                    include_args=(),
+                    define_args="PMAC={{ pmac.P }}",
                 ),
             ),
+            script=(),
         ),
     ),
 )
@@ -36,5 +114,9 @@ def test_deserialize_support() -> None:
 
 
 def test_format_script_on_entity() -> None:
-    pmac = SUPPORT.entities[0]
-    assert pmac.format_script(PORT="blah") == "PMACPortConfigure(blah, 100)​"
+    with open(Path(__file__).parent / "pmac.ibek.yaml") as f:
+        actual = Support.deserialize(YAML().load(f))
+
+    pmacIP = actual.entities[1]
+    script = pmacIP.format_script(port="blah", IP="1.1.1.1")
+    assert script == "PMACAsynIPPort(blah, 1.1.1.1:1025)"
