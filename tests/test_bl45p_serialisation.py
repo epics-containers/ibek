496fbd9764f87ac5003539a5f158b6907b80ca4d
rparke <50015370+rparke@users.noreply.github.com>
2021-05-18 14:25:19 +0100

    updated test_bl45p_serialisation.py

--- tests/test_bl45p_serialisation.py
+++ tests/test_bl45p_serialisation.py
@@ -5,6 +5,7 @@ from ruamel.yaml import YAML
 from ibek.pmac import DlsPmacAsynMotor, Geobrick, PmacAsynIPPort, PmacIOC
 
 BL45P_MO_IOC_02 = PmacIOC(
+    ioc_name="BL45P_MO_IOC_02",
     instances=(
         PmacAsynIPPort(
             name="BRICK1port", type="pmac.PmacAsynIPPort", IP="192.168.0.12:1112"
@@ -39,7 +40,7 @@ BL45P_MO_IOC_02 = PmacIOC(
             P="BL45P-MO-MIRR-01:X1",
             axis=1,
         ),
-    )
+    ),
 )
 
 
