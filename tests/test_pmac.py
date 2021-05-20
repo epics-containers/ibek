47639e4ac4925953551beab7c4536fd8d3808be9
rparke <50015370+rparke@users.noreply.github.com>
2021-05-20 10:22:30 +0100

    test corrected to include deserialize method

--- tests/test_pmac.py
+++ tests/test_pmac.py
@@ -1,13 +1,13 @@
 import json
 
 import apischema
-from apischema.json_schema import serialization_schema
+from apischema.json_schema import deserialization_schema
 
 from ibek.pmac import PmacIOC
 
 
 def test_make_schema() -> None:
-    my_schema = serialization_schema(PmacIOC)
+    my_schema = deserialization_schema(PmacIOC)
     with open("/tmp/pmac.schema.json", "w") as f:
         json.dump(my_schema, f)
 
