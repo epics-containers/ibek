3a557f1f026adba28cf9975144743f2db3aab420
rparke <50015370+rparke@users.noreply.github.com>
2021-05-24 09:32:22 +0100

    updated tests

--- tests/test_pmac.py
+++ tests/test_pmac.py
@@ -1,13 +1,15 @@
 import json
 
 import apischema
-from apischema.json_schema import serialization_schema
+from apischema.json_schema import deserialization_schema
 
 from ibek.pmac import PmacIOC
 
 
 def test_make_schema() -> None:
-    my_schema = serialization_schema(PmacIOC)
-    with open("/tmp/pmac.schema.json", "w") as f:
+    my_schema = deserialization_schema(PmacIOC)
+    with open(
+        "/Users/richardparke/Documents/K8-IOCs/ibek/tests/pmac.schema.json", "w"
+    ) as f:
         json.dump(my_schema, f)
 
