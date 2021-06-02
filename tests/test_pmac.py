db90901777e304c2eed3a8e4cc94686d18929b82
rparke <50015370+rparke@users.noreply.github.com>
2021-06-02 12:28:26 +0100

    added tests for pmac.py

--- tests/test_pmac.py
+++ tests/test_pmac.py
@@ -3,13 +3,45 @@ import json
 import apischema
 from apischema.json_schema import deserialization_schema
 
-from ibek.pmac import PmacIOC
+from ibek.pmac import DatabaseEntry, EntityInstance, Geobrick, PmacAsynIPPort, PmacIOC
 
 
-def test_make_schema() -> None:
-    my_schema = deserialization_schema(PmacIOC)
-    with open(
-        "/Users/richardparke/Documents/K8-IOCs/ibek/tests/pmac.schema.json", "w"
-    ) as f:
-        json.dump(my_schema, f)
+def test_create_database():
+    """ creates a simple instance of a database and checks that a startup script entry of the 
+    correct form is returned """
+    ei = EntityInstance(name="Test Entity", script=["my name is: {{ name }}"])
+    db = (
+        DatabaseEntry(
+            file='"database_entry_one.template"', define_args='"name = {{ name }}"'
+        ),
+    )
+    assert (
+        ei.create_database(db)[0].render(ei.__dict__)
+        == 'dbLoadRecords("database_entry_one.template", "name = Test Entity")'
+    )
+
+
+def test_pmac_asyn_ipport_script():
+    pmac_asyn_ipport_instance = PmacAsynIPPort(
+        name="my_pmac_instance", IP="111.111.111.111"
+    )
+    assert pmac_asyn_ipport_instance.create_scripts() == [
+        "pmacAsynIPConfigure(my_pmac_instance, 111.111.111.111:1025)"
+    ]
+
+
+def test_geobrick_script():
+    pmac_geobrick_instance = Geobrick(
+        name="test_geobrick",
+        port="my_asyn_port",
+        P="geobrick_one",
+        idlePoll=200,
+        movingPoll=800,
+    )
+
+    print(pmac_geobrick_instance.create_scripts())
+
+
+def test_geobrick_database():
+    pass
 
