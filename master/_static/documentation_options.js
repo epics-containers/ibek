a4101386e97ce19e0b519f479734b040b86e14cc
rparke <rparke@users.noreply.github.com>
2021-06-11 15:32:10 +0000

    deploy: e32cb3c2da57cc61476a13940ee1a305388cfffa

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0+untagged.g5afb3e6',
+    VERSION: '0.0+untagged.ge32cb3c',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
