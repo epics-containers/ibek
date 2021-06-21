5331ba2df7cf5800f4a89cb8db6ac851d789f96f
rparke <50015370+rparke@users.noreply.github.com>
2021-06-21 15:22:13 +0100

    fixed issue with EntityInstance subclasses

--- ibek/BL45P-MO-IOC-01/dataclass_from_yaml.py
+++ ibek/BL45P-MO-IOC-01/dataclass_from_yaml.py
@@ -1,3 +1,4 @@
+import json
 from dataclasses import dataclass
 
 from apischema.json_schema import deserialization_schema
@@ -11,19 +12,28 @@ class yaml_to_dataclass:
     yaml_file: str
 
     def _get_support_instance(self) -> Support:
+        """ Deserializes a yaml file into an instance of the Support class """
         yaml = YAML()
         with open(self.yaml_file, "r") as f:
             return Support.deserialize(yaml.load(f))
 
-    def get_module_dataclass(self):
-        support_instance = self._get_support_instance()
-        module_dataclass = support_instance.get_module()
-        return module_dataclass
+    def get_module_dataclass(self) -> type:
+        """ Creates a dataclass as described in self.yaml_file """
+        # support_instance = self._get_support_instance()
+        # module_dataclass = support_instance.get_module()
+        return self._get_support_instance().get_module()
 
 
 test = yaml_to_dataclass(
     "/Users/richardparke/Documents/K8-IOCs/ibek/tests/pmac.ibek.yaml"
 )
 
-print(deserialization_schema(test.get_module_dataclass()))
+
+# print(type(test.get_module_dataclass()))
+example = deserialization_schema(test.get_module_dataclass())
+
+
+# with open("/tmp/schema.json", "w") as f:
+#     json.dump(deserialization_schema(test.get_module_dataclass()), f)
+# # print(deserialization_schema(test.get_module_dataclass()))
 
