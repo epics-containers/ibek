a8b46e69415f3ed9078a7d2698bc923271b32ede
Giles Knap <giles.knap@diamond.ac.uk>
2023-06-27 13:42:09 +0100

    fixed YAML generation

--- builder2ibek.support.py
+++ builder2ibek.support.py
@@ -1,20 +1,16 @@
 #!/bin/env dls-python
 import re
+import sys
 
 class_name_re = re.compile(r"class '.*\.(.*)'")
 description_re = re.compile(r"(.*)\n<type")
 
-MODULE_TREE = {}
 
-
-def get_arg_details(args, arg_name, builder_class, default=None):
+def arg_details(args, arg_name, builder_class, default=None):
     arg = builder_class.ArgInfo.descriptions[arg_name]
 
     matches = description_re.findall(arg.desc)
-    if len(matches) > 0:
-        description = matches[0]
-    else:
-        description = "NO DESCRIPTION"
+    description = matches[0] if len(matches) > 0 else "NO DESCRIPTION"
 
     if arg_name == getattr(builder_class, "UniqueName", "name"):
         typ = "id"
@@ -31,19 +27,28 @@ def get_arg_details(args, arg_name, builder_class, default=None):
     else:
         typ = "UNKNOWN"
 
-    arg = {"type": typ, "name": arg_name, "description": description}
+    arg = ordereddict()
+    arg["type"], arg["name"], arg["description"] = typ, arg_name, description
+    if default is not None:
+        arg["default"] = default
 
     args.append(arg)
 
 
 def main():
+    if len(sys.argv) != 2:
+        print("Usage: %s <path to support module>" % sys.argv[0])
+        sys.exit(1)
+
+    release_file = sys.argv[1] + "/configure/RELEASE"
+
+    MODULE_TREE = ordereddict()
+
     Configure()
     ImportFunctions()
 
     # load builder modules for a single support module ADCore
-    tree = dependency_tree(
-        None, "/dls_sw/prod/R3.14.12.7/support/ADCore/3-9dls2/configure/RELEASE"
-    )
+    tree = dependency_tree(None, release_file)
 
     for leaf in tree.leaves:
         ModuleVersion(leaf.name, leaf.version)
@@ -59,27 +64,30 @@ def main():
             and not name.startswith("_")
             and not name.endswith("Template")
         ):
-            this_def = {}
+            this_def = ordereddict()
             defs.append(this_def)
             this_def["name"] = name
-            this_def["description"] = "TODO: add description"
+            this_def["description"] = "TODO TODO TODO add description"
             args = this_def["args"] = []
 
             for arg_name in builder_class.ArgInfo.required_names:
-                get_arg_details(args, arg_name, builder_class)
+                arg_details(args, arg_name, builder_class)
 
             for arg_name in builder_class.ArgInfo.optional_names:
-                get_arg_details(args, arg_name, builder_class, "UNDEFINED")
+                arg_details(args, arg_name, builder_class, "UNDEFINED")
 
             for index, arg_name in enumerate(builder_class.ArgInfo.default_names):
-                get_arg_details(
+                arg_details(
                     args,
                     arg_name,
                     builder_class,
                     builder_class.ArgInfo.default_values[index],
                 )
 
-    print(MODULE_TREE)
+    yaml = YAML()
+    yaml.default_flow_style = False
+
+    yaml.dump(MODULE_TREE, sys.stdout)
 
 
 if __name__ == "__main__":
@@ -88,12 +96,14 @@ if __name__ == "__main__":
 
     require("iocbuilder")
     require("dls_dependency_tree")
-    require("PyYAML")
+    require("ruamel.yaml")
 
     # import required modules
     from dls_dependency_tree import dependency_tree
     from iocbuilder.configure import Configure
     from iocbuilder.libversion import ModuleVersion
     from iocbuilder.mydbstatic import ImportFunctions
+    from ruamel.yaml import YAML
+    from ruamel.yaml.comments import CommentedMap as ordereddict
 
     main()
