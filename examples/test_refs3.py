479c330456358c9af514713137f18648e8e246e4
Giles Knap <giles.knap@diamond.ac.uk>
2023-07-17 14:39:15 +0000

    another example of refs - more like ioc.py

---
+++ examples/test_refs3.py
@@ -0,0 +1,90 @@
+from __future__ import annotations
+
+from typing import Dict, Literal, Optional, Sequence, Union
+
+from pydantic import (
+    BaseModel,
+    ConfigDict,
+    Field,
+    RootModel,
+    create_model,
+    field_validator,
+    model_validator,
+)
+
+id_to_entity: Dict[str, Entity] = {}
+
+
+class Entity(BaseModel):
+    type: Literal["e"]
+    name: str = Field(..., description="The name of this entity")
+    value: str = Field(..., description="The value of this entity")
+    ref: Optional[str] = Field(
+        default=None, description="Reference another Entity name"
+    )
+    model_config = ConfigDict(extra="forbid")
+
+    @model_validator(mode="after")  # type: ignore
+    def add_ibek_attributes(cls, entity: Entity):
+        id_to_entity[entity.name] = entity
+
+        return entity
+
+
+@field_validator("ref", mode="after")
+def lookup_instance(cls, id):
+    try:
+        return id_to_entity[id]
+    except KeyError:
+        raise KeyError(f"object {id} not found in {list(id_to_entity)}")
+
+
+validators = {"Entity": lookup_instance}
+
+# add validator to the Entity class using create model
+Entity2 = create_model(
+    "Entity",
+    __validators__=validators,
+    __base__=Entity,
+)  # type: ignore
+
+entity_models = [Entity2, Entity2]
+
+
+class EntityModel(RootModel):
+    root: Union[tuple(entity_models)] = Field(discriminator="type")  # type: ignore
+
+
+class Entities(BaseModel):
+    model_config = ConfigDict(extra="forbid")
+    entities: Sequence[EntityModel] = Field(  # type: ignore
+        description="List of entities this IOC instantiates"
+    )
+
+
+model1 = Entities(
+    **{
+        "entities": [
+            {"type": "e", "name": "one", "value": "OneValue"},
+            {"type": "e", "name": "two", "value": "TwoValue", "ref": "one"},
+        ]
+    }
+)
+
+# demonstrate that entity one has a reference to entity two
+assert model1.entities[1].root.ref.value == "OneValue"
+
+# this should throw an error because entity one has illegal arguments
+model2 = Entities(
+    **{
+        "entities": [
+            {
+                "type": "e",
+                "name": "one",
+                "value": "OneValue",
+                "illegal": "bad argument",
+            },
+            {"type": "e", "name": "two", "value": "TwoValue", "ref": "one"},
+        ]
+    }
+)
