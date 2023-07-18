94de03d7b5a83169e3ec8e981e2daf1826f23ba1
gilesknap <gilesknap@users.noreply.github.com>
2023-07-18 07:34:33 +0000

    deploy: 99b5ab8cbc5e3bca295fa472f31fdb4ce6f726da

--- pydantic2/_static/documentation_options.js
+++ pydantic2/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.9.6.dev137+gb44b119',
+    VERSION: '0.9.6.dev139+g99b5ab8',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
