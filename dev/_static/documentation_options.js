f190e3d5daf8f71ab6019fe3da5ba01afc9fe587
gilesknap <gilesknap@users.noreply.github.com>
2023-09-26 20:25:26 +0000

    deploy: c712c8239bd98995d544ae6491e053e87fcf9fd3

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.14.dev32+g23cd5d7',
+    VERSION: '0.0.14.dev32+gc712c82',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
