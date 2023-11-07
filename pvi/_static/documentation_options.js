70384636c9c1d966ad4785802cbd46004e0346be
GDYendell <GDYendell@users.noreply.github.com>
2023-11-07 15:53:39 +0000

    deploy: 93ced512dd930d019f9825a81c73166ad4e4f373

--- pvi/_static/documentation_options.js
+++ pvi/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.4.1.dev13+g8af9664a',
+    VERSION: '1.4.3.dev3+g93ced512',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
@@ -8,7 +8,7 @@ var DOCUMENTATION_OPTIONS = {
     LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: false,
+    NAVIGATION_WITH_KEYS: true,
     SHOW_SEARCH_SUMMARY: true,
     ENABLE_SEARCH_SHORTCUTS: true,
 };
