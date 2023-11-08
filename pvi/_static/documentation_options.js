bb31a23da0c7292c6e4ff2573c82a116d8afd3d7
GDYendell <GDYendell@users.noreply.github.com>
2023-11-08 11:26:36 +0000

    deploy: 8c4dc5c64dad500cde149d43b7921fb0025fba3c

--- pvi/_static/documentation_options.js
+++ pvi/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.4.3.dev3+g93ced512',
+    VERSION: '1.4.3.dev3+g8c4dc5c6',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
