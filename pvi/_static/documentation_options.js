e6b048ca4f7c29d95e6a07f189204943bab234c8
gilesknap <gilesknap@users.noreply.github.com>
2023-11-09 08:45:48 +0000

    deploy: 21889bda88e625b7a87dc846a5f8938bc4185e23

--- pvi/_static/documentation_options.js
+++ pvi/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.4.3.dev6+g2e7a0a9d',
+    VERSION: '1.5.0b2.dev1+g21889bda',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
