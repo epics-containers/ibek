84b5e51145d7733e6095aa3b70646563844ef7c9
gilesknap <gilesknap@users.noreply.github.com>
2024-01-05 15:00:00 +0000

    deploy: 37e6f5efadc835af1c57a1003b6868a3611ad52c

--- docker/_static/documentation_options.js
+++ docker/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.5.5.dev6+gb33b3019',
+    VERSION: '1.5.5.dev6+g37e6f5ef',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
