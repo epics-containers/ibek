a4e8e739fdb5d17d9637c0206de2247f6dc92fd6
gilesknap <gilesknap@users.noreply.github.com>
2024-01-16 14:05:29 +0000

    deploy: d5720280d65b31b51d8d14ef034a8796e7226d0c

--- remove-ioc-template/_static/documentation_options.js
+++ remove-ioc-template/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.6.0b2.dev1+gec2a744b',
+    VERSION: '1.5.5b2.dev4+gd5720280',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
