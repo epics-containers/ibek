d30e26a293cd5c85e1ba75ac06abfd5d7c09bdff
Giles Knap <giles.knap@diamond.ac.uk>
2024-01-17 10:40:13 +0000

    fix ibek ioc instance

--- src/ibek/dev_cmds/commands.py
+++ src/ibek/dev_cmds/commands.py
@@ -13,7 +13,11 @@ dev_cli = typer.Typer(cls=NaturalOrderGroup)
 @dev_cli.command()
 def instance(
     instance: Path = typer.Argument(
-        ..., help="The filepath to the ioc instance entity file"
+        ...,
+        help="The filepath to the ioc instance entity file",
+        dir_okay=True,
+        file_okay=False,
+        exists=True,
     ),
 ):
     """
@@ -23,35 +27,28 @@ def instance(
     /epics/ioc/starts.sh. Changes made to the config will be immediately
     available and also under version control.
 
-    e.g. if instance is /repos/bl38p/iocs/bl38p-mo-panda-01 then we need:
-    - /epics/ioc -> /epics/ioc-pandablocks/ioc
-    - /epics/ioc/config -> /repos/bl38p/iocs/bl38p-mo-panda-01/config
+    e.g. if instance is /workspaces/bl38p/iocs/bl38p-mo-panda-01 then we need:
+    - /epics/ioc/config -> /workspaces/bl38p/iocs/bl38p-mo-panda-01/config
     """
 
     # validate the instance folder has a config folder
     ioc_folder = IOC_FOLDER
-    config_folder = ioc_folder / CONFIG_DIR_NAME
+    config_folder = IOC_FOLDER / CONFIG_DIR_NAME
     instance_config = instance / CONFIG_DIR_NAME
 
-    if not instance_config.exists():
-        log.error(f"Could not find config folder {instance_config}")
+    # verify that the expected folder exists
+    if not ioc_folder.exists():
+        log.error(f"Could not find ioc folder {ioc_folder}")
         raise typer.Exit(1)
 
-    # First make sure there is an ioc folder in the generic IOC source root
-    # and that it is symlinked from /epics/ioc, remove any existing config.
-    if not ioc_folder.exists():
-        ioc_folder.mkdir()
+    # remove any existing config folder from /epics/ioc
+    if config_folder.is_symlink():
+        config_folder.unlink()
+    elif config_folder.exists():
+        shutil.rmtree(config_folder)
 
-    # remove any existing config folder and ioc symlink from /epics/ioc
-    for folder in [config_folder, IOC_FOLDER]:
-        if folder.is_symlink():
-            folder.unlink()
-        if folder.exists():
-            shutil.rmtree(folder)
-
-    IOC_FOLDER.symlink_to(ioc_folder)
-
-    # Now symlink the instance config folder into the generic IOC source root
+    # Now symlink the instance config folder into /epics/ioc
+    print(f"Symlinking {instance_config} to {config_folder}")
     config_folder.symlink_to(instance_config)
 
 
