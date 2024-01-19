b4742dbd92ab08996119e9dc477ef6b4d2e82af9
gilesknap <gilesknap@users.noreply.github.com>
2024-01-19 10:15:13 +0000

    deploy: 4dee77c61094fe375662de60a53c3263be2935db

--- fix-extract-assets/_static/documentation_options.js
+++ fix-extract-assets/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.6.1.dev1+g02ca980c',
+    VERSION: '1.6.1.dev1+g4dee77c6',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
