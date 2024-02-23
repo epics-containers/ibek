a407d93931eb2e78bd85df5b62b26524ccf70331
GDYendell <GDYendell@users.noreply.github.com>
2024-02-23 10:39:10 +0000

    deploy: cfc7dcd529bb8898e9fc406b82a418892878277e

--- pv-macros/_static/documentation_options.js
+++ pv-macros/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.6.4.dev2+gb761fe6f',
+    VERSION: '1.7.0b2.dev1+gcfc7dcd5',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
