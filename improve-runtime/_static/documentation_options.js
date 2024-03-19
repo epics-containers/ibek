5e17d55ee39153567bfa18b7be84e025b1a1a7a6
gilesknap <gilesknap@users.noreply.github.com>
2024-03-19 22:48:46 +0000

    deploy: dd8667a42a277ca1cc8ba11dc70f77fdaf334a50

--- improve-runtime/_static/documentation_options.js
+++ improve-runtime/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.7.3.dev6+g96561f19',
+    VERSION: '1.8.1b2.dev1+gdd8667a4',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
