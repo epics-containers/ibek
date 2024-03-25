0afaa8b114c93dc14e01ccdb57f969afa63c9858
gilesknap <gilesknap@users.noreply.github.com>
2024-03-25 15:58:49 +0000

    deploy: 470e3e198910ea6a4be611f5bbd4f39f4403c522

--- RTEMS/_static/documentation_options.js
+++ RTEMS/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.8.2b4.dev2+g6e337801',
+    VERSION: '1.8.2b4.dev3+g470e3e19',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
