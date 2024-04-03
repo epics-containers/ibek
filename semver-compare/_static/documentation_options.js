92e0d3b33e68421f09e79fc5c4d69e42e6a09cc9
marcelldls <marcelldls@users.noreply.github.com>
2024-04-03 14:57:17 +0000

    deploy: 0b93daebb04aea2e0c4c718b346994db9c500835

--- semver-compare/_static/documentation_options.js
+++ semver-compare/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.0.2.dev5+gdb28c9e2',
+    VERSION: '2.0.2.dev6+g0b93daeb',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
