6fe0d149d3f6dd73a5890c4b001b82f7acb21e33
marcelldls <marcelldls@users.noreply.github.com>
2024-04-03 08:50:01 +0000

    deploy: e615565b2e07ae39c767e114bba673194d7ea936

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.0.1.dev2+g928f52e2',
+    VERSION: '2.0.2.dev1+ge615565b',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
