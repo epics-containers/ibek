009410ae6939fd79f05244c181281d8d6d257130
gilesknap <gilesknap@users.noreply.github.com>
2024-05-16 08:19:34 +0000

    deploy: 2b4e7324555c05d44ed02a5398b461903eafad1a

--- motor-demo2/_static/documentation_options.js
+++ motor-demo2/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.5.0b2.dev10+gdfb17441',
+    VERSION: '3.5.0b2.dev11+g2b4e7324',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
