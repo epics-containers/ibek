5434b8d4a139f5ca714c9eddfb03f3df8961f713
marcelldls <marcelldls@users.noreply.github.com>
2024-05-21 08:26:52 +0000

    deploy: 881dd79ddbec04636baeab855529719cf0d69067

---
+++ add-systest-guard/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '3.0.0b4.dev1+g881dd79d',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: true,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
