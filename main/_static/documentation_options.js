78ad7e2fb2e4ef6f7363fabc8435edfbbe5b889d
gilesknap <gilesknap@users.noreply.github.com>
2024-05-21 10:08:48 +0000

    deploy: 0f0e33209f2027d127596e16640b916b0f771630

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '3.0.0b3.dev3+g4cc996d6',
+    VERSION: '3.0.0b4.dev2+g0f0e3320',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
