6778ae3ac68cc36f93e04471dc136e94bb52e576
gilesknap <gilesknap@users.noreply.github.com>
2024-05-24 16:12:30 +0000

    deploy: 65344ee1a0c0bfd16640c04a9da13730a6bc2c34

--- motor-demo2/_static/documentation_options.js
+++ motor-demo2/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '3.0.0b5.dev2+ge7f44499',
+    VERSION: '3.0.0b5.dev3+g65344ee1',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
