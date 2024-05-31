af3bf434f854e50b4ede7b1b41002dc4c8b9836f
gilesknap <gilesknap@users.noreply.github.com>
2024-05-31 08:56:20 +0000

    deploy: 556839765b90e727b922c22f92d678f8d4f6507b

--- schema-review/_static/documentation_options.js
+++ schema-review/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '3.0.0b6.dev4+gb714d200',
+    VERSION: '3.0.0b6.dev5+g55683976',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
