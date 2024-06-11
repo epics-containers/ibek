d1426b78bd70ee9194520ffc42cebfdd5352a66e
gilesknap <gilesknap@users.noreply.github.com>
2024-06-11 15:17:17 +0000

    deploy: 7db1e35d1772269148408b81d6c927618b369d70

--- improve-jinja/_static/documentation_options.js
+++ improve-jinja/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '3.0.0b6.dev32+ga7a101c5',
+    VERSION: '3.0.0b6.dev34+g7db1e35d',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
