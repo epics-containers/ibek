3d29aaacb1e2e17549278eea36491e25211bf1e9
gilesknap <gilesknap@users.noreply.github.com>
2024-06-12 14:32:02 +0000

    deploy: f2099f4a261c455e4eb6f382941654146a5f2b4a

--- list-arg2dict-param/_static/documentation_options.js
+++ list-arg2dict-param/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '3.0.0b6.dev42+g4e2c7f13',
+    VERSION: '3.0.0b6.dev43+gf2099f4a',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
